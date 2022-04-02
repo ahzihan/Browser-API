416173e70c91d50bf07e29d10fc0808fd8dac7d1
Md Akbar Hossain <akbarhossain160@gmail.com>
2022-04-02 16:23:19 +0600

    first commit

---
+++ shopping.js
@@ -0,0 +1,54 @@
+const displayLocalstorageCard = () => {
+    const cart = getCart();
+    for ( const name in cart ) {
+        displayProduct( name );
+    }
+};
+
+
+const addItem = () => {
+    const nameField = document.getElementById( 'product-name' );
+    const name = nameField.value;
+    nameField.value = '';
+    if ( !name ) {
+        return;
+    }
+    //display Product
+    displayProduct( name );
+    //add to local storage
+    addProductToCart( name );
+
+};
+
+const placeOrder = () => {
+    document.getElementById( 'products' ).textContent = '';
+    localStorage.removeItem( 'cart' );
+};
+const displayProduct = name => {
+    const ul = document.getElementById( 'products' );
+    const li = document.createElement( 'li' );
+    li.innerText = name;
+    ul.appendChild( li );
+};
+const getCart = () => {
+    const cart = localStorage.getItem( 'cart' );
+    let cartObj;
+    if ( cart ) {
+        cartObj = JSON.parse( cart );
+    } else {
+        cartObj = {};
+    }
+    return cartObj;
+};
+const addProductToCart = name => {
+    const cart = getCart();
+    if ( cart[ name ] ) {
+        cart[ name ] += 1;
+    } else {
+        cart[ name ] = 1;
+    }
+
+    const cartStringified = JSON.stringify( cart );
+    localStorage.setItem( 'cart', cartStringified );
+};
+displayLocalstorageCard();
